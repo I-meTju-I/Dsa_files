5afffd13c267d5fafee213290002305006efe625
MatusHrkel <matushrkel724gmail.com>
2023-02-20 18:19:45 +0100

    Tree template for DSA

---
+++ tree_template.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int value;
+    struct Node *left;
+    struct Node *right;
+}NODE;
+
+void search(NODE *root){
+
+}
+
+void insert(NODE **root){
+    int v;
+    NODE *p;
+    NODE *temp;
+    scanf("Value of new node: %d",&v);
+
+    temp->value = v;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    if(*root == NULL){
+        *root = temp;
+    }
+    p = *root;
+    if(){
+
+    }
+}
+
+void delete(NODE *root){
+
+}
+
+int create(NODE *root){
+
+}
+
+void main(){
+    NODE *root = NULL;
+    char input;
+
+    scanf("%c",&input);
+
+    while(input != 'q'){
+        switch (input){
+        case 's':
+            search(root);
+            break;
+        case 'c':
+            create(&root);
+            break;
+        case 'd':
+            delete(&root);
+            break;
+        case 'i':
+            insert(&root);
+            break;
+        default:
+            break;
+        }
+    }
+}
