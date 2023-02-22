aa4ca3a046f23d6b22cc67b548c6ca15d5f3c606
MatusHrkel <matushrkel724gmail.com>
2023-02-22 23:35:05 +0100

    Tree template- new functions

--- tree_template.c
+++ tree_template.c
@@ -1,69 +1,162 @@
+//Tree template without self balancing coded 
+//|-meTju-|
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
-typedef struct Node{
+typedef struct node{
     int value;
-    struct Node *left;
-    struct Node *right;
+    struct node *left;
+    struct node *right;
 }NODE;
 
-void search(NODE *root){
-    int val;
+int search(int val,NODE *root){
     NODE *temp;
-    scanf("%d",&val);
 
     temp = root;
+    if(temp == NULL){
+        printf("No node with value [%d] is in tree\n",val);
+        return 0;
+    }
     if(temp->value == val){
-        printf("Node value: %d\nNode adress: %p\n",temp->value,temp);
+        printf("Node with value [%d] exists on adress: %p\n",temp->value,temp);
+        return 1;
+    }
+    if(val < temp->value){
+        search(val,temp->left);
+    }
+    if(val > temp->value){
+        search(val,temp->right);
     }
 }
 
-void insert(NODE **root){
-    int v;
-    NODE *p;
+int insert(int val,NODE **root){
     NODE *temp;
-    scanf("Value of new node: %d",&v);
+    NODE *new;
 
-    temp->value = v;
-    temp->left = NULL;
-    temp->right = NULL;
+    new = (NODE *)malloc(sizeof(NODE));
+    new->value = val;
+    new->left = NULL;
+    new->right = NULL;
 
     if(*root == NULL){
-        *root = temp;
+        *root = new;
+        return 1;
     }
-    p = *root;
+
+    temp = *root;
+    if(val < temp->value){
+        insert(val,&temp->left);
+    }
+    if(val > temp->value){
+        insert(val,&temp->right);
+    }
 }
 
-void delete(NODE *root){
+//WIP
+int delete(int val,NODE **root){
+    NODE *temp;
+    NODE *garb;
+
+    temp = *root;
+    if(temp == NULL){
+        printf("No node with value [%d] is in tree\n",val);
+        return 0;
+    }
+    //Left child node
+    if(temp->left->value == val){
+        //Case 1
+        if(temp->left->right == NULL && temp->left->left == NULL){
+            free(temp->left);
+            temp->left = NULL;
+        }
+        //Case 2
+        if(temp->left->right != NULL){
+            garb = temp->left;
+            temp->left = temp->left->right;
+            free(garb);
+        }else if(temp->left->left != NULL){
+            garb = temp->left;
+            temp->left = temp->left->left;
+            free(garb);
+        }
+        //Case 3 WIP...
+        return 1;
+    }
+    //Right child node
+    if(temp->right->value == val){
+        //Case 1
+        if(temp->right->right == NULL && temp->right->left == NULL){
+            free(temp->right);
+            temp->right = NULL;
+        }
+        //Case 2
+        if(temp->right->right != NULL){
+            garb = temp->right;
+            temp->right = temp->right->right;
+            free(garb);
+        }else if(temp->right->left != NULL){
+            garb = temp->right;
+            temp->right = temp->right->left;
+            free(garb);
+        }
+        //Case 3 WIP...
+        return 1;
+    }
+    if(val < temp->value){
+        delete(val,&temp->left);
+    }
+    if(val > temp->value){
+        delete(val,&temp->right);
+    }
 
 }
 
-int create(NODE *root){
-
+int create(int val,NODE **root){
+    int number;
+    srand(time(0));
+    for(int i = 0;i < val;i++){
+        number = rand() % 100;
+        printf("Creating node with number.. %d\n",number);
+        insert(number,root);
+    }
 }
 
 void main(){
     NODE *root = NULL;
+    int val;
     char input;
 
+    printf("Select your operation - s = search, c = create, i = insert, d = delete, q = quit: ");
     scanf("%c",&input);
 
     while(input != 'q'){
         switch (input){
         case 's':
-            search(root);
+            printf("Value you wish to search for: ");
+            scanf("%d",&val);
+            search(val,root);
             break;
         case 'c':
-            create(&root);
+            printf("Number of new nodes: ");
+            scanf("%d",&val);
+            create(val,&root);
             break;
         case 'd':
-            delete(&root);
+            printf("Value you wish to delete: ");
+            scanf("%d",&val);
+            delete(val,&root);
             break;
         case 'i':
-            insert(&root);
+            printf("Value of new node: ");
+            scanf("%d",&val);
+            insert(val,&root);
             break;
         default:
             break;
         }
+        printf("Your operation? s = search, c = create, i = insert, d = delete, q = quit: ");
+        scanf(" %c",&input);
     }
+    printf("\nQuitting...\n");
 }
