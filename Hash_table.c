10a2c30d54fb0bf7ad553cf7002484ea3d9e52dc
I-meTju-I <matushrkel724@gmail.com>
2023-03-13 23:23:21 +0100

    Completed hash table template, added documentation

--- Hash_table.c
+++ Hash_table.c
@@ -1,32 +1,35 @@
-//Hash table
+//Hash table template without any collision handling
 //|-meTju-|
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
 
+//Item struct containing key and name
 typedef struct h_item{
     char *key, *name;
 }ITEM;
 
-
+//Hash table struct containing array of items and info about itself
 typedef struct table{
     ITEM **array;
     long count,size;
 }HASH_TABLE;
 
-long hash(char *key){
+//Temporary hash function
+long hash(char *key,int size){
     long hash = 5381;
     int c;
 
     while (c = *key++){
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + c;
     }
-    return hash;
+    return hash % size;
 }
 
+//Allocates memory for ITEM and fills it with args
 ITEM *create_item(char *key, char *name){
-    ITEM * new;
+    ITEM *new;
 
     new = (ITEM *)malloc(sizeof(ITEM));
     new->key = (char *)malloc(sizeof(char)*strlen(key) + 1);
@@ -37,6 +40,8 @@ ITEM *create_item(char *key, char *name){
     return new;
 }
 
+//Creates new hash table at the start of program
+//and allocates memory for it
 HASH_TABLE create_hashtable(int size){
     HASH_TABLE new;
     new.array = (ITEM **)calloc(size,sizeof(ITEM *));
@@ -50,36 +55,124 @@ HASH_TABLE create_hashtable(int size){
     return new;
 }
 
-void insert(HASH_TABLE *table){
-    int hash_key;
-    char *key,*name;
+//Basic insert
+void insert(HASH_TABLE *table,char *key,char *name){
+    int index;
     ITEM *new;
-
-    scanf("Your key: %s\n Your name: %s\n",key,name);
-    hash_key = hash(key);
+    
+    index = hash(key,table->size);
     new = create_item(key,name);
 
-    table->array[hash_key] = new;
+    table->array[index] = new;
+    table->count += 1;
+}
+
+//Basic search
+void search(HASH_TABLE table){
+    char key[30];
+    int index;
+    printf("Key that you would like to search for: ");
+    scanf("%s",key);
+    index = hash(key,table.size);
+    if(table.array[index]== NULL){
+        printf("No key with value [%s] in hash table\n",key);
+    }else{
+        printf("Key exists on index [%d]. Key: [%s] and name: [%s]\n",index,table.array[index]->key,table.array[index]->name);
+    }
+}
+
+//Basic delete
+void delete(HASH_TABLE *table){
+    char key[30];
+    int index;
+    printf("Key that you would like to delete: ");
+    scanf("%s",key);
+    index = hash(key,table->size);
+
+    if(table->array[index] == NULL){
+        printf("No key with value [%s] in hash table\n",key);
+    }else{
+        free(table->array[index]->key);     //Memory deallocation
+        free(table->array[index]->name);
+        free(table->array[index]);
+        table->array[index] = NULL;
+        printf("Deleted key with value [%s] in hash table\n",key);
+    }
+}
+
+//Prints not NULL indexes in hash table
+void print_table(HASH_TABLE table){
+    for(int i = 0; i < table.size;i++){
+        if(table.array[i] != NULL){
+            printf("On index [%d] is key: [%s] and name: [%s]\n",i,table.array[i]->key,table.array[i]->name);
+        }
+    }
+}
+
+//Creates random string, argument length can be max 32
+void rand_string(char string[],int len){
+    char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";        
+
+    if (len > 0){          
+        for (int i = 0;i < len;i++){            
+            int key = rand() % 61;
+            string[i] = chars[key];
+        }
+        string[len] = '\0';
+    }
+}
+
+//Cycles through insert() val times with random keys and names
+void create(HASH_TABLE *table,int val){
+    char key[17];
+    char name[17];
+    srand(time(0));
+
+    for(int i = 0;i < val;i++){
+        rand_string(key,16);
+        rand_string(name,16);
+        insert(table,key,name);
+    }
 }
 
 void main(){
     char input;
+    char *key, *name;
     HASH_TABLE table;
-    table = create_hashtable(10);
+    clock_t start,end;
+    double cpu_time;
+    int val;
+    table = create_hashtable(5);
     
     printf("Select your operation - (s)earch, (c)reate, (i)nsert, (d)elete, (p)rint, (q)uit: ");
     scanf(" %c",&input);
     while(input != 'q'){
         switch (input){
         case 'i':
-            insert(&table);
+            printf("Your key: ");
+            scanf("%s",key);
+            printf("Your name: ");
+            scanf("%s",name);
+            insert(&table,key,name);
             break;
         case 'd':
+            delete(&table);
             break;
         case 's':
+            search(table);
             break;
         case 'c':
+            printf("Number of new keys: ");
+            scanf("%d",&val);
+            start = clock();
+            create(&table,val);
+            end = clock();
+            cpu_time = (double)(end - start)/CLOCKS_PER_SEC;
+            printf("Time taken: %lf sec.\n",cpu_time);
             break;
+            break;
+        case 'p':
+            print_table(table);
         default:
             break;
         }
